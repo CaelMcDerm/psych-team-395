fb345e87c98c835011b506e0bb5d50e7a5bf5894
CaelMcDerm <caelmcdermott@gmail.com>
2026-04-10 10:02:26 -0700

    Delete merged directory

--- merged/backend/routes/groups.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from config import SPECIES_GROUPS
-
-groups_bp = Blueprint("groups", __name__)
-
-
-@groups_bp.route("/api/groups", methods=["GET"])
-def get_groups():
-    return jsonify(SPECIES_GROUPS)
